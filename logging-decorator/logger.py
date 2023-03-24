634bbab33a89be7e24c33f966fb5ca65c48ddf49
Biagio Piraino <biagiopirainowork@gmail.com>
2023-03-24 14:01:17 +0000

    formatted scripts

--- logging-decorator/logger.py
+++ logging-decorator/logger.py
@@ -1,11 +1,11 @@
 from loggerDO import LoggerDO
 
 class logit:
-    def __init__(self, function) -> None:
-        self.function = function
+  def __init__(self, function) -> None:
+    self.function = function
 
-    def __call__(self, *args: any, **kwargs: any) -> any:
-        with LoggerDO() as db:
-            db.LogMethodCall(self.function.__name__, kwargs["user"])
-
-        return self.function(*args, **kwargs)
+  def __call__(self, *args: any, **kwargs: any) -> any:
+    with LoggerDO() as db:
+      db.LogMethodCall(self.function.__name__, kwargs["user"])
+    
+    return self.function(*args, **kwargs)
--- logging-decorator/loggerDO.py
+++ logging-decorator/loggerDO.py
@@ -4,30 +4,30 @@ from user import User
 
 
 class LoggerDO:
-    def __init__(self):
-        self.host     = os.environ.get("HOST")
-        self.port     = os.environ.get("PORT")
-        self.dbname   = os.environ.get("DB_NAME")
-        self.user     = os.environ.get("USER")
-        self.password = os.environ.get("PASSWORD")
+  def __init__(self):
+    self.host     = os.environ.get("HOST")
+    self.port     = os.environ.get("PORT")
+    self.dbname   = os.environ.get("DB_NAME")
+    self.user     = os.environ.get("USER")
+    self.password = os.environ.get("PASSWORD")
 
-    def __enter__(self):
-        self.conn = psycopg2.connect(
-            host     = self.host,
-            port     = self.port,
-            dbname   = self.dbname,
-            user     = self.user,
-            password = self.password
-        )
-        self.cursor = self.conn.cursor()
-        return self
+  def __enter__(self):
+    self.conn  = psycopg2.connect(
+      host     = self.host,
+      port     = self.port,
+      dbname   = self.dbname,
+      user     = self.user,
+      password = self.password
+    )
+    self.cursor = self.conn.cursor()
+    return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.cursor.close()
-        self.conn.close()
+  def __exit__(self, exc_type, exc_val, exc_tb):
+    self.cursor.close()
+    self.conn.close()
 
-    def LogMethodCall(self, method_name: str, user: User) -> None:
-        self.cursor.callproc(
-            "log_method_call",
-            (method_name, user.username, user.authenticated))
-        self.conn.commit()
+  def LogMethodCall(self, method_name: str, user: User) -> None:
+    self.cursor.callproc(
+        "log_method_call",
+        (method_name, user.username, user.authenticated))
+    self.conn.commit()
